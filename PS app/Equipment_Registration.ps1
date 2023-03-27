a0101125a91f735b39e0b193acdaf16a672613b5
Andres Perez <andresperezfirigua@gmail.com>
2023-03-26 19:50:23 -0500

    New computer addition bug fixed

--- PS app/Equipment_Registration.ps1
+++ PS app/Equipment_Registration.ps1
@@ -1,6 +1,23 @@
 Add-Type -AssemblyName System.Windows.Forms
 Add-Type -AssemblyName System.Drawing
+# Hide the console window
+Add-Type -Name Window -Namespace ConsoleApp -MemberDefinition '
+[DllImport("Kernel32.dll")]
+public static extern IntPtr GetConsoleWindow();
 
+[DllImport("user32.dll")]
+public static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+public static void Hide()
+{
+    IntPtr hWnd = GetConsoleWindow();
+    if (hWnd != IntPtr.Zero)
+    {
+        // 0 = Hide the window
+        ShowWindow(hWnd, 0);
+    }
+}'
+[ConsoleApp.Window]::Hide()
 
 function Clean_Controls {
     $textboxSearch.Text = ""
@@ -22,13 +39,13 @@ function Add_Non_Asurion_Device ($outputFilePath) {
     }
     function ValidateTextbox {
         if ([string]::IsNullOrWhiteSpace($textboxSerial.Text)) {
-            [System.Windows.Forms.MessageBox]::Show("Debe especificar el seríal del equipo", "Error", "OK", "Error")
+            [System.Windows.Forms.MessageBox]::Show([System.Text.RegularExpressions.Regex]::Unescape("Indique el n\u00FAmero serial del equipo"), "Error", "OK", "Error")
             return $true
         } elseif ([string]::IsNullOrWhiteSpace($textboxModel.Text)) {
-            [System.Windows.Forms.MessageBox]::Show("Debe especificar el modelo del equipo", "Error", "OK", "Error")
+            [System.Windows.Forms.MessageBox]::Show("Indique el modelo o referencia del equipo", "Error", "OK", "Error")
             return $true
         } elseif ([string]::IsNullOrWhiteSpace($textboxBearer.Text)) {
-            [System.Windows.Forms.MessageBox]::Show("Debe especificar quien lleva el equipo", "Error", "OK", "Error")
+            [System.Windows.Forms.MessageBox]::Show([System.Text.RegularExpressions.Regex]::Unescape("Indique qui\u00E9n lleva el equipo"), "Error", "OK", "Error")
             return $true
         }
         return $false
@@ -36,7 +53,7 @@ function Add_Non_Asurion_Device ($outputFilePath) {
     # Create a form
     $form = New-Object System.Windows.Forms.Form
     $form.Text = ""
-    $form.Size = New-Object System.Drawing.Size(300, 220)
+    $form.Size = New-Object System.Drawing.Size(400, 280)
     $form.StartPosition = "CenterScreen"
     $form.ControlBox = $false
     $form.MaximizeBox = $false
@@ -44,79 +61,81 @@ function Add_Non_Asurion_Device ($outputFilePath) {
 
     # Create labels and textboxes
     $labelSerial = New-Object System.Windows.Forms.Label
-    $labelSerial.Location = New-Object System.Drawing.Point(10, 20)
+    $labelSerial.Location = New-Object System.Drawing.Point(20, 20)
     $labelSerial.Size = New-Object System.Drawing.Size(100, 20)
-    $labelSerial.Text = "Numero serial:"
+    $labelSerial.Text = [System.Text.RegularExpressions.Regex]::Unescape("N\u00FAmero serial:")
     $form.Controls.Add($labelSerial)
 
     $textboxSerial = New-Object System.Windows.Forms.TextBox
-    $textboxSerial.Location = New-Object System.Drawing.Point(120, 20)
-    $textboxSerial.Size = New-Object System.Drawing.Size(160, 20)
+    $textboxSerial.Location = New-Object System.Drawing.Point(150, 20)
+    $textboxSerial.Size = New-Object System.Drawing.Size(200, 20)
     $form.Controls.Add($textboxSerial)
 
     $labelModel = New-Object System.Windows.Forms.Label
-    $labelModel.Location = New-Object System.Drawing.Point(10, 50)
+    $labelModel.Location = New-Object System.Drawing.Point(20, 60)
     $labelModel.Size = New-Object System.Drawing.Size(100, 20)
     $labelModel.Text = "Modelo:"
     $form.Controls.Add($labelModel)
 
     $textboxModel = New-Object System.Windows.Forms.TextBox
-    $textboxModel.Location = New-Object System.Drawing.Point(120, 50)
-    $textboxModel.Size = New-Object System.Drawing.Size(160, 20)
+    $textboxModel.Location = New-Object System.Drawing.Point(150, 60)
+    $textboxModel.Size = New-Object System.Drawing.Size(200, 20)
     $form.Controls.Add($textboxModel)
 
     $labelType = New-Object System.Windows.Forms.Label
-    $labelType.Location = New-Object System.Drawing.Point(10, 80)
-    $labelType.Size = New-Object System.Drawing.Size(100, 20)
+    $labelType.Location = New-Object System.Drawing.Point(20, 100)
+    $labelType.Size = New-Object System.Drawing.Size(120, 20)
     $labelType.Text = "Tipo de dispositivo:"
     $form.Controls.Add($labelType)
 
     $dropdownType = New-Object System.Windows.Forms.ComboBox
-    $dropdownType.Location = New-Object System.Drawing.Point(120, 80)
-    $dropdownType.Size = New-Object System.Drawing.Size(160, 20)
-    $dropdownType.Items.Add("Portátil")
+    $dropdownType.Location = New-Object System.Drawing.Point(150, 100)
+    $dropdownType.Size = New-Object System.Drawing.Size(200, 20)
+    $dropdownType.DropDownStyle = [System.Windows.Forms.ComboBoxStyle]::DropDownList
+    $dropdownType.Items.Add("")
+    $dropdownType.Items.Add("PC / Laptop")
     $dropdownType.Items.Add("Otro")
+    $dropdownType.SelectedIndex = 0
     $form.Controls.Add($dropdownType)
 
     $labelBearer = New-Object System.Windows.Forms.Label
-    $labelBearer.Location = New-Object System.Drawing.Point(10, 110)
-    $labelBearer.Size = New-Object System.Drawing.Size(100, 20)
+    $labelBearer.Location = New-Object System.Drawing.Point(20, 140)
+    $labelBearer.Size = New-Object System.Drawing.Size(200, 20)
     $labelBearer.Text = "Nombre de quien lleva el equipo:"
     $form.Controls.Add($labelBearer)
 
     $textboxBearer = New-Object System.Windows.Forms.TextBox
-    $textboxBearer.Location = New-Object System.Drawing.Point(120, 110)
-    $textboxBearer.Size = New-Object System.Drawing.Size(160, 20)
+    $textboxBearer.Location = New-Object System.Drawing.Point(220, 140)
+    $textboxBearer.Size = New-Object System.Drawing.Size(130, 20)
     $form.Controls.Add($textboxBearer)
 
     $labelAdditional = New-Object System.Windows.Forms.Label
-    $labelAdditional.Location = New-Object System.Drawing.Point(10, 140)
-    $labelAdditional.Size = New-Object System.Drawing.Size(100, 20)
-    $labelAdditional.Text = "Informacion adicional:"
+    $labelAdditional.Location = New-Object System.Drawing.Point(20, 180)
+    $labelAdditional.Size = New-Object System.Drawing.Size(200, 20)
+    $labelAdditional.Text = [System.Text.RegularExpressions.Regex]::Unescape("Informaci\u00F3n adicional:")
     $form.Controls.Add($labelAdditional)
 
     $textboxAdditional = New-Object System.Windows.Forms.TextBox
-    $textboxAdditional.Location = New-Object System.Drawing.Point(120, 140)
-    $textboxAdditional.Size = New-Object System.Drawing.Size(160, 20)
+    $textboxAdditional.Location = New-Object System.Drawing.Point(220, 180)
+    $textboxAdditional.Size = New-Object System.Drawing.Size(130, 20)
     $form.Controls.Add($textboxAdditional)
 
     # Create a button for bearer
     $buttonAddDevice = New-Object System.Windows.Forms.Button
-    $buttonAddDevice.Location = New-Object System.Drawing.Point(10, 160)
+    $buttonAddDevice.Location = New-Object System.Drawing.Point(90, 230)
     $buttonAddDevice.Size = New-Object System.Drawing.Size(200, 30)
     $buttonAddDevice.Text = "Registrar"
     $buttonAddDevice.Add_Click({
         if (-not (ValidateTextbox)){
             $newComputer = @()
             $newComputer += $textboxSerial.Text
-            Write-Host "Serial asignado a newComp var $newComputer"
             $newComputer += "N/A"
             $newComputer += "N/A"
             $newComputer += $textboxModel.Text
-            if (-not ($null -eq $dropdownType.SelectedItem)) {
+            if ((-not ($dropdownType.SelectedItem -eq ""))) {
                 $newComputer += $dropdownType.SelectedItem
             } else {
-                [System.Windows.Forms.MessageBox]::Show("Debe seleccionar el tipo dispositivo", "Error", "OK", "Error")
+                [System.Windows.Forms.MessageBox]::Show("Seleccione el tipo de dispositivo", "Error", "OK", "Error")
                 return
             }
             $newComputer += $dateString
@@ -128,10 +147,8 @@ function Add_Non_Asurion_Device ($outputFilePath) {
             } else {
                 $newComputer += $textboxAdditional.Text
             }
-            Write-Host "newComp var antes de terminar evento $newComputer"
             $form.Close()
             $form.Dispose()
-            Write-Host $newComputer.GetType().Name
             Add_Record_To_File $outputFilePath $newComputer
         }
     })
@@ -166,7 +183,7 @@ function Find_Computer($inputFilePath, $searchValue) {
             # If the search value is found, get the values in the row and copy them to the array
             for ($j = 1; $j -le $worksheet.UsedRange.Columns.Count; $j++) {
                 if (($j -eq 3) -and ($worksheet.Cells.Item($i, 3).Value2 -eq "")) {
-                    $foundItem += "Equipo no asignado"
+                    $foundItem += "No asignado"
                 } else {
                     $cell = $worksheet.Cells.Item($i, $j).Value2
                     $foundItem += $cell
@@ -218,7 +235,7 @@ function Add_Record_To_File ($outputFilePath, $foundItem) {
 function Get_Different_Bearer ($foundItem) {
     function ValidateBearer {
         if ([string]::IsNullOrWhiteSpace($textboxBearer.Text)) {
-            [System.Windows.Forms.MessageBox]::Show("Debe especificar quien lleva el equipo", "Error", "OK", "Error")
+            [System.Windows.Forms.MessageBox]::Show([System.Text.RegularExpressions.Regex]::Unescape("Indique qui\u00E9n lleva el equipo"), "Error", "OK", "Error")
             return $true
         }
         return $false
@@ -226,7 +243,7 @@ function Get_Different_Bearer ($foundItem) {
     # Create a form
     $formBearer = New-Object System.Windows.Forms.Form
     $formBearer.Text = ""
-    $formBearer.Size = New-Object System.Drawing.Size(230, 120)
+    $formBearer.Size = New-Object System.Drawing.Size(237, 178)
     $formBearer.StartPosition = "CenterScreen"
     $formBearer.ControlBox = $false
     $formBearer.MaximizeBox = $false
@@ -234,19 +251,19 @@ function Get_Different_Bearer ($foundItem) {
 
     # Create a label and text box for bearer
     $labelBearer = New-Object System.Windows.Forms.Label
-    $labelBearer.Location = New-Object System.Drawing.Point(10, 20)
+    $labelBearer.Location = New-Object System.Drawing.Point(19, 50)
     $labelBearer.Size = New-Object System.Drawing.Size(200, 20)
-    $labelBearer.Text = "Quien lleva el equipo?"
+    $labelBearer.Text = [System.Text.RegularExpressions.Regex]::Unescape("Qui\u00E9n lleva el equipo?")
     $formBearer.Controls.Add($labelBearer)
 
     $textboxBearer = New-Object System.Windows.Forms.TextBox
-    $textboxBearer.Location = New-Object System.Drawing.Point(10, 40)
+    $textboxBearer.Location = New-Object System.Drawing.Point(19, 80)
     $textboxBearer.Size = New-Object System.Drawing.Size(200, 20)
     $formBearer.Controls.Add($textboxBearer)
 
     # Create a button for bearer
     $buttonBearer = New-Object System.Windows.Forms.Button
-    $buttonBearer.Location = New-Object System.Drawing.Point(10, 70)
+    $buttonBearer.Location = New-Object System.Drawing.Point(19, 110)
     $buttonBearer.Size = New-Object System.Drawing.Size(200, 30)
     $buttonBearer.Text = "OK"
     $buttonBearer.Add_Click({
@@ -272,7 +289,7 @@ function Record_Equipment {
     # $inputFilePath = "C:\Users\Peter.Cadwell\Downloads\alm_hardware.xlsx"
     # $outputFilePath = "C:\Users\Peter.Cadwell\Downloads\Minuta_computadores.xlsx"
     if ([string]::IsNullOrWhiteSpace($textboxSearch.Text)) {
-        [System.Windows.Forms.MessageBox]::Show("Debe ingresar un equipo para buscar", "Error", "OK", "Error")
+        [System.Windows.Forms.MessageBox]::Show("Ingrese un equipo para buscar", "Error", "OK", "Error")
         return
     }
     $searchToUpper = $textboxSearch.Text.ToUpper()
@@ -281,7 +298,7 @@ function Record_Equipment {
     $foundItem = Find_Computer $inputFilePath $searchValue
 
     if ($null -ne $foundItem){
-        if (-not ($foundItem[2] -eq "Equipo no asignado")) {
+        if (-not ($foundItem[2] -eq "No asignado")) {
             $bearer = $foundItem[7]
             $dialogResult = [System.Windows.Forms.MessageBox]::Show("Es $bearer quien tiene el equipo a registrar?", "Confirmation", "YesNoCancel", "Question")
 
@@ -300,7 +317,7 @@ function Record_Equipment {
                 Clean_Controls
             }
         } else {
-            [System.Windows.Forms.MessageBox]::Show("Equipo no asignado en inventario", "Error", "OK", "Error")
+            [System.Windows.Forms.MessageBox]::Show([System.Text.RegularExpressions.Regex]::Unescape("Este equipo a\u00FAn no est\u00E1 asignado en inventario"), "Error", "OK", "Error")
             $modifiedFoundItem = Get_Different_Bearer $foundItem
 
             Add_Record_To_File $outputFilePath $modifiedFoundItem
@@ -309,7 +326,7 @@ function Record_Equipment {
             Clean_Controls
         }
     } else {
-        [System.Windows.Forms.MessageBox]::Show("El equipo no se encuentra en inventario", "Error", "OK", "Error")
+        [System.Windows.Forms.MessageBox]::Show("El equipo no fue encontrado. Posibles razones:`n`n- Puede que no sea un PC o una laptop`n- Puede ser propiedad de Asurion pero en otro pais`n- Puede no ser propiedad de Asurion`n`n De clic en OK para registrarlo.", "Error", "OK", "Error")
         Add_Non_Asurion_Device $outputFilePath
         # Display a message to the user indicating that the data was added successfully
         [System.Windows.Forms.MessageBox]::Show("Registro agregado exitosamente.", "", "OK", "Information")
@@ -327,31 +344,31 @@ $form.FormBorderStyle = [System.Windows.Forms.FormBorderStyle]::FixedSingle
 
 # Create a label and text box for search value
 $labelSearch = New-Object System.Windows.Forms.Label
-$labelSearch.Location = New-Object System.Drawing.Point(10, 20)
+$labelSearch.Location = New-Object System.Drawing.Point(25, 20)
 $labelSearch.Size = New-Object System.Drawing.Size(200, 20)
 $labelSearch.Text = "Ingrese el equipo a buscar:"
 $form.Controls.Add($labelSearch)
 
 $textboxSearch = New-Object System.Windows.Forms.TextBox
-$textboxSearch.Location = New-Object System.Drawing.Point(10, 40)
+$textboxSearch.Location = New-Object System.Drawing.Point(25, 40)
 $textboxSearch.Size = New-Object System.Drawing.Size(200, 20)
 $form.Controls.Add($textboxSearch)
 
 $radioIngreso = New-Object System.Windows.Forms.RadioButton
-$radioIngreso.Location = New-Object System.Drawing.Point(10, 70)
+$radioIngreso.Location = New-Object System.Drawing.Point(25, 70)
 $radioIngreso.Size = New-Object System.Drawing.Size(200, 30)
 $radioIngreso.Text = "Ingreso de equipo"
 $form.Controls.Add($radioIngreso)
 
 $radioRetiro = New-Object System.Windows.Forms.RadioButton
-$radioRetiro.Location = New-Object System.Drawing.Point(10, 100)
+$radioRetiro.Location = New-Object System.Drawing.Point(25, 100)
 $radioRetiro.Size = New-Object System.Drawing.Size(200, 30)
 $radioRetiro.Text = "Retiro de equipo"
 $form.Controls.Add($radioRetiro)
 
 # Create a button for Registrar equipo
 $buttonRegistro = New-Object System.Windows.Forms.Button
-$buttonRegistro.Location = New-Object System.Drawing.Point(10, 140)
+$buttonRegistro.Location = New-Object System.Drawing.Point(25, 140)
 $buttonRegistro.Size = New-Object System.Drawing.Size(200, 30)
 $buttonRegistro.Text = "Registrar"
 $buttonRegistro.Add_Click({
@@ -359,7 +376,7 @@ $buttonRegistro.Add_Click({
             Record_Equipment
         }
         else {
-            [System.Windows.Forms.MessageBox]::Show("Debe especificar si el equipo ingresa o sale del piso", "Error", "OK", "Error")
+            [System.Windows.Forms.MessageBox]::Show("Indique si el equipo ingresa o sale del piso", "Error", "OK", "Error")
         }
     })
 $form.Controls.Add($buttonRegistro)
