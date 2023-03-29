3ca34531cf2d3849ac625e6572a2f701d33e0b38
Andres Perez <andresperezfirigua@gmail.com>
2023-03-28 20:13:05 -0500

    New version of Coffee Machine Project

--- Coffee-Machine-Project/main.py
+++ Coffee-Machine-Project/main.py
@@ -1,79 +1,67 @@
 from Dict_Data import MENU
 from Dict_Data import resources
 
-def print_report(machine_money):
-    print(f"Water: {resources['water']}\nMilk: {resources['milk']}\nCoffee: {resources['coffee']}\nMoney: {machine_money}")
-
 
 def check_resources(drink):
-    if MENU[drink]["ingredients"]["water"] <= resources["water"]:
-        if MENU[drink]["ingredients"]["coffee"] <= resources["coffee"]:
-            if drink == "espresso":
-                return None
-            elif MENU[drink]["ingredients"]["milk"] <= resources["milk"]:
-                return None
-            else:
-                return "milk"
-        else:
-            return "coffee"
-    else:
-        return "water"
+    for ingredient in MENU[drink]["ingredients"]:
+        if resources[ingredient] < MENU[drink]["ingredients"][ingredient]:
+            print(f"Sorry there is not enough {ingredient}.")
+            return False
+    return True
 
 
-def prepare_order(drink):
-    resources["water"] -= MENU[drink]["ingredients"]["water"]
-    resources["coffee"] -= MENU[drink]["ingredients"]["coffee"]
-    if drink != "espresso":
-        resources["milk"] -= MENU[drink]["ingredients"]["milk"]
-
-
-def process_transaction(drink):
+def process_coins():
+    coins = {
+        "quarters": 0.25,
+        "dimes": 0.10,
+        "nickles": 0.05,
+        "pennies": 0.01
+    }
     payment = 0
-    quarters = int(input("How many quarters?: "))
-    payment += quarters * 0.25
-    if payment >= MENU[drink]["cost"]:
-        return payment
-    dimes = int(input("How many dimes?: "))
-    payment += dimes * 0.10
-    if payment >= MENU[drink]["cost"]:
-        return payment
-    nickles = int(input("How many nickels?: "))
-    payment += nickles * 0.05
-    if payment >= MENU[drink]["cost"]:
-        return payment
-    pennies = int(input("How many pennies?: "))
-    payment += pennies * 0.01
-    if payment >= MENU[drink]["cost"]:
-        return payment
+    print("Please insert coins")
+    for coin in coins:
+        inserted_coins = int(input(f"How many {coin}?: "))
+        payment += inserted_coins * coins[coin]
+    return payment
+
+
+def check_transaction(payment, drink):
+    drink_cost = MENU[drink]["cost"]
+    if payment > drink_cost:
+        print(f"Here is ${round(payment - drink_cost, 2)} dollars in change.")
+        return True
+    elif payment == drink_cost:
+        return True
     else:
-        return -1
+        print("Sorry, that's not enough money. Money refunded.")
+        return False
 
 
-def turn_machine_on():
-    serve_next_order = True
+def make_coffee(drink):
+    for ingredient in MENU[drink]["ingredients"]:
+        resources[ingredient] -= MENU[drink]["ingredients"][ingredient]
+    print(f"Here is your {drink}. Enjoy!")
+
+
+def print_report(machine_money):
+    print(f"Water: {resources['water']}\nMilk: {resources['milk']}\nCoffee: {resources['coffee']}\nMoney: ${machine_money}")
+
+
+def turn_on():
+    user_input = ""
     machine_money = 0
-    while serve_next_order:
-        print(resources, machine_money)
-        drink = input("What would you like? (espresso/latte/cappuccino): ").lower()
-        if drink in ["espresso", "latte", "cappuccino"]:
-            if check_resources(drink) is None:
-                inserted_money = process_transaction(drink)
-                if inserted_money != -1:
-                    prepare_order(drink)
-                    machine_money += MENU[drink]['cost']
-                    if inserted_money > MENU[drink]["cost"]:
-                        print(f"Here's ${round(inserted_money - MENU[drink]['cost'], 2)} in change.")
-                    print(f"Here's your {drink}. Enjoy!")
-                else:
-                    print(f"Sorry that's not enough money. Money refunded")
-            else:
-                print(f"Sorry there is not enough {check_resources(drink)}.")
-        elif drink == "off":
-            serve_next_order = False
-        elif drink == "report":
+    while user_input != "off":
+        user_input = input("What would you like? (espresso/latte/cappuccino): ").lower()
+        if user_input in ["espresso", "latte", "cappuccino"]:
+            if check_resources(user_input):
+                payment = process_coins()
+                if check_transaction(payment, user_input):
+                    machine_money += MENU[user_input]["cost"]
+                    make_coffee(user_input)
+        elif user_input == "report":
             print_report(machine_money)
         else:
-            print("Invalid option.")
+            print("Invalid option")
 
 
-turn_machine_on()
+turn_on()
