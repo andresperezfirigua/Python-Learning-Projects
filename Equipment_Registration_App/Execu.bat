b64b69b50cb392eafa47b5a71be7d25576cb2d17
Andres Perez <andresperezfirigua@gmail.com>
2023-04-03 16:41:10 -0500

    Organized files and folders for deployment

--- Equipment_Registration_App/Execu.bat
+++
@@ -1,7 +0,0 @@
-# cd ".\Downloads"
-# powershell "& 'C:\Users\Peter.Cadwell\Downloads\GUI test.ps1'"
-
-cd ".\PycharmProjects\PS app"
-powershell "& 'C:\Users\andresfelipe.perez\PycharmProjects\PS app\GUI test.ps1'"
-
-pause
