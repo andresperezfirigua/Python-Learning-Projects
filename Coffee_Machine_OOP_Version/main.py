11df3bd3daf829b35eaf96c80f4d7d141d6991ef
Andres Perez <andresperezfirigua@gmail.com>
2023-04-03 19:06:17 -0500

    Latest changes to OOP version added

--- Coffee_Machine_OOP_Version/main.py
+++ Coffee_Machine_OOP_Version/main.py
@@ -1,31 +1,3 @@
-# #import turtle
-# from turtle import Turtle, Screen
-#
-# timmy = Turtle()
-# print(timmy)
-# timmy.shape("turtle")
-# timmy.color("coral")
-# timmy.forward(100)
-#
-# my_screen = Screen()
-# print(my_screen.canvheight)
-# my_screen.exitonclick()
-#
-# from prettytable import PrettyTable
-#
-# table = PrettyTable()
-#
-# table.field_names = ["Pokemon Name", "Type"]
-# table.add_rows(
-#     [
-#         ["Pikachu", "Electric"],
-#         ["Squirtle", "Water"],
-#         ["Charmander", "Fire"]
-#     ]
-# )
-#
-# print(table)
-
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
@@ -46,8 +18,8 @@ def turn_on():
             drink = menu.find_drink(usr_input)
             if drink is not None:
                 if coffeeMaker.is_resource_sufficient(drink):
-
-
+                    if moneyMachine.make_payment(drink.cost):
+                        coffeeMaker.make_coffee(drink)
 
 
 turn_on()
