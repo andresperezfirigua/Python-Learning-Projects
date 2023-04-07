365a9a7656d3b554a996d932ff1e0bed64679a87
Andres Perez <andresperezfirigua@gmail.com>
2023-04-07 16:05:46 -0500

    Completed Hirst painting project using Turtle graphics

--- Hirst_Painting_Project/main.py
+++ Hirst_Painting_Project/main.py
@@ -19,7 +19,12 @@
 '''
 from turtle import Turtle, Screen
 import random
-color_list = [(132, 166, 205), (221, 148, 106), (32, 42, 61), (199, 135, 148), (166, 58, 48), (141, 184, 162), (39, 105, 157), (237, 212, 90), (150, 59, 66), (216, 82, 71), (168, 29, 33), (235, 165, 157), (51, 111, 90), (35, 61, 55), (156, 33, 31), (17, 97, 71), (52, 44, 49), (230, 161, 166), (170, 188, 221), (57, 51, 48), (184, 103, 113), (32, 60, 109), (105, 126, 159), (175, 200, 188), (34, 151, 210), (65, 66, 56), (106, 140, 124), (153, 202, 227), (48, 69, 71), (131, 128, 121)]
+
+color_list = [(132, 166, 205), (221, 148, 106), (32, 42, 61), (199, 135, 148), (166, 58, 48), (141, 184, 162),
+              (39, 105, 157), (237, 212, 90), (150, 59, 66), (216, 82, 71), (168, 29, 33), (235, 165, 157),
+              (51, 111, 90), (35, 61, 55), (156, 33, 31), (17, 97, 71), (52, 44, 49), (230, 161, 166), (170, 188, 221),
+              (57, 51, 48), (184, 103, 113), (32, 60, 109), (105, 126, 159), (175, 200, 188), (34, 151, 210),
+              (65, 66, 56), (106, 140, 124), (153, 202, 227), (48, 69, 71), (131, 128, 121)]
 
 tommy = Turtle()
 screen = Screen()
@@ -27,17 +32,32 @@ tommy.shape("classic")
 tommy.speed(0)
 tommy.color("sea green")
 tommy.penup()
-tommy.setposition(-400, -300)
+screen_size = screen.screensize()
+tommy.setposition(-(screen_size[0] - 50), -(screen_size[1]))
+init_position = (tommy.pos()[0], tommy.pos()[1])
+current_position = init_position
 screen.colormode(255)
-x = 0
-y = 0
-def generate_color():
+
+
+def generate_dot_color():
     return random.choice(color_list)
-def draw_dot():
-    color = generate_color()
+
+
+def draw_dot_line():
+    color = generate_dot_color()
     tommy.dot(20, color)
     tommy.forward(50)
-#def start_new_line():
-for _ in range(11):
-    draw_dot()
+    return tommy.pos()
+
+
+def start_new_line(current_position):
+    tommy.setposition(init_position[0], current_position[1] + 50)
+    return tommy.pos()
+
+
+while current_position[1] <= screen_size[1]:
+    while current_position[0] < screen_size[0]:
+        current_position = draw_dot_line()
+    current_position = start_new_line(current_position)
+
 screen.exitonclick()
