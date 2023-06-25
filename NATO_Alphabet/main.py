0ac981d85a6b94b3c3e570bdeaa254e092298858
Andres <andresperezfirigua@gmail.com>
2023-06-24 19:23:51 -0500

    Adding exception hadling to the word search inside the alphabet dictionary

--- NATO_Alphabet/main.py
+++ NATO_Alphabet/main.py
@@ -6,6 +6,22 @@ alphabet_dataframe = pandas.read_csv('nato_phonetic_alphabet.csv')
 alphabet_dictionary = {row.letter: row.code for (index, row) in alphabet_dataframe.iterrows()}
 
 # TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-word = input('Enter a word: ').upper()
-nato_translation = [alphabet_dictionary[letter] for letter in word if letter in alphabet_dictionary]
-print(nato_translation)
+
+
+def generate_phonetic_translation():
+    word = input('Enter a word: ').upper()
+    try:
+        nato_translation = [alphabet_dictionary[letter] for letter in word]
+        if len(nato_translation) == 0:
+            raise ValueError('Empty field')
+    except KeyError:
+        print('Sorry, only letters in the alphabet please.')
+        generate_phonetic_translation()
+    except ValueError:
+        print("Field can't be empty, please enter a word")
+        generate_phonetic_translation()
+    else:
+        print(nato_translation)
+
+
+generate_phonetic_translation()
