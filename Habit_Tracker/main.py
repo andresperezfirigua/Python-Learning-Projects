6581e4bab3889db90f291f400d595813861ad13b
Andres <andresperezfirigua@gmail.com>
2023-07-24 18:05:08 -0500

    Completed project using HTTP Post, Put and Delete methods to interact with Pixela API

--- Habit_Tracker/main.py
+++ Habit_Tracker/main.py
@@ -1,7 +1,11 @@
+import datetime
+
 import requests
 
-USERNAME = 'andresperezfirigua'
-TOKEN = 'sfvisyb45t34r3jb453bj'
+# Creating an account in Pixela using an HTTP Post request
+
+USERNAME = 'Create a username here'
+TOKEN = 'Create a token here'
 pixela_endpoint = 'https://pixe.la/v1/users'
 
 user_params = {
@@ -9,11 +13,15 @@ user_params = {
     'notMinor': 'yes'
 }
 
-#response = requests.post(url=pixela_endpoint, json=user_params)
-#print(response.text)
+# response = requests.post(url=pixela_endpoint, json=user_params)
+# print(response.text)
+
+# Creating a graph in my Pixela account using an HTTP Post request
+
+graph_id = 'graph1'
 
 graph_config = {
-    'id': 'graph1',
+    'id': graph_id,
     'name': 'Coding Graph',
     'unit': 'commit',
     'type': 'int',
@@ -26,6 +34,40 @@ headers = {
 
 graph_endpoint = f'{pixela_endpoint}/{USERNAME}/graphs'
 
-response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+# response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
 
-print(response.text)
+# print(response.text)
+
+# Creating a pixel in my Pixela graph using an HTTP Post request
+
+# date = datetime.datetime(year=2023, month=7, day=22).strftime("%Y%m%d")
+date = datetime.datetime.now().strftime("%Y%m%d")
+
+pixel_config = {
+    'date': date,
+    'quantity': '10'
+}
+
+create_pixel_endpoint = f'{graph_endpoint}/{graph_id}'
+
+# response = requests.post(url=create_pixel_endpoint, json=pixel_config, headers=headers)
+
+# print(response.status_code)
+
+# Updating a pixel in my Pixela graph using an HTTP Put request
+
+# update_pixel_endpoint = f'{create_pixel_endpoint}/{date}'
+
+update_pixel_config = {
+    'quantity': '8'
+}
+
+# response = requests.put(url=update_pixel_endpoint, json=update_pixel_config, headers=headers)
+# print(response.status_code)
+
+# Deleting a pixel from my Pixela graph using an HTTP Delete request
+
+delete_pixel_endpoint = f'{create_pixel_endpoint}/{date}'
+
+# response = requests.delete(url=delete_pixel_endpoint, headers=headers)
+# print(response.status_code)
