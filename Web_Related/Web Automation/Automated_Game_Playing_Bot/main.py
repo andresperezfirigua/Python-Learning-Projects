9702a3e81c8ad1205af8b252e034c5f618b71491
Andres <andresperezfirigua@gmail.com>
2023-08-30 20:09:00 -0500

    Completed automated playing bot using Selenium, pending minor improvements

--- Web_Related/Web Automation/Automated_Game_Playing_Bot/main.py
+++ Web_Related/Web Automation/Automated_Game_Playing_Bot/main.py
@@ -1,23 +1,34 @@
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 
 driver = webdriver.Chrome()
 driver.get('http://orteil.dashnet.org/experiments/cookie/')
 cookie = driver.find_element(By.ID, 'cookie')
 
-start = time.time()
+current_time = time.time()
+game_duration = current_time + 60
 end = time.time()
-print(time.ctime(start))
-print(time.ctime(start + 5))
-while end < start + 300:
-    pause = end + 5
-    if end < pause:
+next_pause = end + 5
+
+while end < game_duration:
+    if end < next_pause:
         cookie.click()
     else:
-        # Check upgrade availability
-        pass
+        upgrades = driver.find_elements(By.XPATH, '/html/body/div[4]/div[5]/div/div')
+
+        available_upgrades = [upgrade for upgrade in upgrades if upgrade.get_attribute('class') != 'grayed']
+
+        available_upgrade = int(available_upgrades[-1].find_element(By.XPATH, './/b').get_attribute('textContent').split(' ')[-1])
+
+        accumulated_cookies = int(driver.find_element(By.ID, 'money').get_attribute('textContent'))
+
+        if accumulated_cookies > available_upgrade:
+            available_upgrades[-1].click()
+
+        next_pause = end + 5
+
     end = time.time()
 
+print(driver.find_element(By.ID, 'cps').get_attribute('textContent'))
 time.sleep(10)
