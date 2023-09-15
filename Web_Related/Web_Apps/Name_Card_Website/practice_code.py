c8379538e4a67b9b4b112ec2575affb764fc6f0c
Andres <andresperezfirigua@gmail.com>
2023-09-14 20:08:09 -0500

    Adding Name Card Website project, not yet completed

---
+++ Web_Related/Web_Apps/Name_Card_Website/practice_code.py
@@ -0,0 +1,24 @@
+# from flask import Flask, render_template
+#
+# app = Flask(__name__)
+#
+#
+# @app.route('/')
+# def home():
+#     return '<h1>Hello world</h1>'
+#
+# @app.route('/cv')
+# def name_card():
+#     return render_template('cv.html')
+#
+#
+# @app.route('/')
+# def home():
+#     return render_template('test_index.html')
+#
+# @app.route('/')
+# def home():
+#     return render_template('test_index_2.html')
+#
+# if __name__ == '__main__':
+#     app.run(debug=True)
