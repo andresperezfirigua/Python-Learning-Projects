ddf99abe7df3c49ffff3d75737a5a4f4008cacb3
Andres <andresperezfirigua@gmail.com>
2023-10-03 19:52:30 -0500

    Completed Blog project using Jinja templating, dinamic HTML rendering and Bootstrap

--- Web_Related/Web_Apps/Blog/main.py
+++ Web_Related/Web_Apps/Blog/main.py
@@ -1,11 +1,15 @@
+import requests
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
+blog_url = 'https://api.npoint.io/930f7e16959b596f27ec'
+posts = requests.get(blog_url).json()
+
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template("index.html", posts=posts)
 
 
 @app.route('/about')
@@ -16,6 +20,15 @@ def about():
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
+
+
+@app.route('/post/<int:post_id>')
+def post(post_id):
+    requested_post = None
+    for p in posts:
+        if p['id'] == post_id:
+            requested_post = p
+    return render_template('post.html', post=requested_post)
 
 
 if __name__ == "__main__":
