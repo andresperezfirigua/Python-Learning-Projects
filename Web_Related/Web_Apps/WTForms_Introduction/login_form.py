cb530ab7e44239b72790acc4e0f1449152ad4a82
Andres <andresperezfirigua@gmail.com>
2023-10-11 20:04:23 -0500

    Adding WTForms field validators and displaying validation errors to users

--- Web_Related/Web_Apps/WTForms_Introduction/login_form.py
+++ Web_Related/Web_Apps/WTForms_Introduction/login_form.py
@@ -1,7 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import StringField, PasswordField, SubmitField, EmailField
+from wtforms.validators import DataRequired, Length, Email
 
 
 class LoginForm(FlaskForm):
-    email = StringField('Email')
-    password = StringField('Password')
+    email = EmailField(label='Email', validators=[DataRequired(), Email(check_deliverability=True)])
+    password = PasswordField(label='Password', validators=[DataRequired(), Length(min=8)])
+    submit = SubmitField(label='Login')
--- Web_Related/Web_Apps/WTForms_Introduction/main.py
+++ Web_Related/Web_Apps/WTForms_Introduction/main.py
@@ -1,18 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from login_form import LoginForm
 
 app = Flask(__name__)
 
 app.secret_key = 'Vsdkh34[]bd=(&$jJCssjv/%sb635'
 
+
 @app.route("/")
 def home():
     return render_template('index.html')
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    form.validate_on_submit()
     return render_template('login.html', form=form)
 
 
