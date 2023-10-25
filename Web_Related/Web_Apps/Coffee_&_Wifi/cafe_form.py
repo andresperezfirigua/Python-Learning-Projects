d0cfec7aec520cc5f7828d044dd2ed1fc47311b5
Andres <andresperezfirigua@gmail.com>
2023-10-24 19:58:01 -0500

    Finished Coffee & Wifi project using WTForms, Bootstrap-Flask and some CSV manipulation

--- Web_Related/Web_Apps/Coffee_&_Wifi/cafe_form.py
+++ Web_Related/Web_Apps/Coffee_&_Wifi/cafe_form.py
@@ -1,22 +1,52 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, SelectField
 from wtforms.validators import DataRequired, URL
-
-
-# Exercise:
-# add: Location URL, open time, closing time, coffee rating, wifi rating, power outlet rating fields
-# make coffee/wifi/power a select element with choice of 0 to 5.
-#e.g. You could use emojis ☕️/💪/✘/🔌
-# make all fields required except submit
-# use a validator to check that the URL field has a URL entered.
-# ---------------------------------------------------------------------------
 
 class CafeForm(FlaskForm):
     cafe = StringField('Cafe Name', validators=[DataRequired()])
     location = StringField('Location URL', validators=[DataRequired(), URL()])
-    open = StringField('Open Time', validators=[DataRequired()])
+    opening = StringField('Open Time', validators=[DataRequired()])
     closing = StringField('Closing Time', validators=[DataRequired()])
-    coffee_rating = StringField('Coffee Rating', validators=[DataRequired()])
-    wifi_rating = StringField('Wifi Rating', validators=[DataRequired()])
-    power_rating = StringField('Power Outlet Rating', validators=[DataRequired()])
+    coffee_rating = SelectField(
+        'Coffee Rating',
+        choices=[
+            '',
+            '✘',
+            '☕️',
+            '☕️☕️',
+            '☕️☕️☕️',
+            '☕️☕️☕️☕️',
+            '☕️☕️☕️☕️☕️'
+        ],
+        validators=[DataRequired()],
+        validate_choice=False
+    )
+    wifi_rating = SelectField(
+        'Wifi Rating',
+        choices=[
+            '',
+            '✘',
+            '💪',
+            '💪💪',
+            '💪💪💪',
+            '💪💪💪💪',
+            '💪💪💪💪💪'
+        ],
+        validators=[DataRequired()],
+        validate_choice=False
+    )
+    power_rating = SelectField(
+        'Power Outlet Rating',
+        choices=[
+            '',
+            '✘',
+            '🔌',
+            '🔌🔌',
+            '🔌🔌🔌',
+            '🔌🔌🔌🔌',
+            '🔌🔌🔌🔌🔌'
+        ],
+        validators=[DataRequired()],
+        validate_choice=False
+    )
     submit = SubmitField('Submit')
--- Web_Related/Web_Apps/Coffee_&_Wifi/main.py
+++ Web_Related/Web_Apps/Coffee_&_Wifi/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 from flask_bootstrap import Bootstrap5
 from cafe_form import CafeForm
 import csv
@@ -8,7 +8,6 @@ app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 Bootstrap5(app)
 
 
-# all Flask routes below
 @app.route("/")
 def home():
     return render_template("index.html")
@@ -18,10 +17,17 @@ def home():
 def add_cafe():
     form = CafeForm()
     if form.validate_on_submit():
-        print("True")
-    # Exercise:
-    # Make the form write a new row into cafe-data.csv
-    # with   if form.validate_on_submit()
+        with open('cafe-data.csv', mode='a', encoding='utf-8') as csv_file:
+            csv_file.write(
+                f"\n{form.cafe.data},"
+                f"{form.location.data},"
+                f"{form.opening.data},"
+                f"{form.closing.data},"
+                f"{form.coffee_rating.data},"
+                f"{form.wifi_rating.data},"
+                f"{form.power_rating.data}"
+            )
+        return redirect(url_for('cafes'))
     return render_template('add.html', form=form)
 
 
