eaccdbb966d5919f7b3c2279cedd0d7d0054e926
Andres <andresperezfirigua@gmail.com>
2023-11-01 20:00:16 -0500

    Adding some practice code to learn SQLAlchemy ORM to handle database operations

--- Web_Related/Web_Apps/Database_Handling/practice_code.py
+++ Web_Related/Web_Apps/Database_Handling/practice_code.py
@@ -1,9 +1,43 @@
-import sqlite3
+# import sqlite3
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Integer, String, Float
+from sqlalchemy.orm import Mapped, mapped_column
 
-db = sqlite3.connect('testDB.db')
 
-cursor = db.cursor()
+db = SQLAlchemy()
+app = Flask(__name__)
 
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///new_books_collection.db"
+
+db.init_app(app)
+
+
+class Book(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    title: Mapped[str] = mapped_column(String(length=250), unique=True, nullable=False)
+    author: Mapped[str] = mapped_column(String(length=250), nullable=False)
+    rating: Mapped[str] = mapped_column(Float, nullable=False)
+
+
+with app.app_context():
+    db.create_all()
+
+with app.app_context():
+    book = Book(
+        title="Harry Potter",
+        author="J. K. Rowling",
+        rating=9.3
+    )
+
+    db.session.add(book)
+    db.session.commit()
+
+
+# db = sqlite3.connect('testDB.db')
+#
+# cursor = db.cursor()
+#
 # cursor.execute("CREATE TABLE books "
 #                "("
 #                "id INTEGER PRIMARY KEY, "
@@ -12,6 +46,6 @@ cursor = db.cursor()
 #                "NOT NULL, rating FLOAT NOT NULL"
 #                ")"
 #                )
-
-cursor.execute("INSERT INTO books VALUES(1, 'Harry Potter', 'J.K Rowling', '9.3')")
-db.commit()
+#
+# cursor.execute("INSERT INTO books VALUES(1, 'Harry Potter', 'J.K Rowling', '9.3')")
+# db.commit()
