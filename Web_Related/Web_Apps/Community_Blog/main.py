393cd6bafb8bf3b868198d6a43f6d00e8d0f21a6
Andres <andresperezfirigua@gmail.com>
2024-04-15 20:20:15 -0500

    Add route protection so only the admin user can create, edit and delete posts, hid create, edit and delete buttons to non admin users

--- Web_Related/Web_Apps/Community_Blog/main.py
+++ Web_Related/Web_Apps/Community_Blog/main.py
@@ -3,7 +3,7 @@ from flask import Flask, abort, render_template, redirect, url_for, flash
 from flask_bootstrap import Bootstrap5
 from flask_ckeditor import CKEditor
 # from flask_gravatar import Gravatar
-from flask_login import UserMixin, login_user, LoginManager, current_user, logout_user
+from flask_login import UserMixin, login_user, LoginManager, current_user, logout_user, AnonymousUserMixin
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import relationship, DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import Integer, String, Text
@@ -139,7 +139,17 @@ def show_post(post_id):
 
 
 # TODO: Use a decorator so only an admin user can create a new post
+def admin_only(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if AnonymousUserMixin.is_anonymous or current_user.id != 1:
+            abort(403)
+        return f(*args, **kwargs)
+    return decorated_function
+
+
 @app.route("/new-post", methods=["GET", "POST"])
+@admin_only
 def add_new_post():
     form = CreatePostForm()
     if form.validate_on_submit():
@@ -159,6 +169,7 @@ def add_new_post():
 
 # TODO: Use a decorator so only an admin user can edit a post
 @app.route("/edit-post/<int:post_id>", methods=["GET", "POST"])
+@admin_only
 def edit_post(post_id):
     post = db.get_or_404(BlogPost, post_id)
     edit_form = CreatePostForm(
@@ -181,6 +192,7 @@ def edit_post(post_id):
 
 # TODO: Use a decorator so only an admin user can delete a post
 @app.route("/delete/<int:post_id>")
+@admin_only
 def delete_post(post_id):
     post_to_delete = db.get_or_404(BlogPost, post_id)
     db.session.delete(post_to_delete)
